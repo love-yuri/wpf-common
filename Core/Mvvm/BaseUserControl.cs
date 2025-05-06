22ab7e7d4e15c615fd6887b40c0cccfa4b6a1cfa
love-yuri <yuri2078170658@gmail.com>
2025-05-06 15:43:00 +0800

    feat: 自动注入ViewModel到BaseUserControl

--- Core/Mvvm/BaseUserControl.cs
+++ Core/Mvvm/BaseUserControl.cs
@@ -1,21 +1,23 @@
 ﻿using System;
 using System.Windows.Controls;
+using LoveYuri.Core.Di;
 
 namespace LoveYuri.Core.Mvvm {
     /// <summary>
     /// 显示界面基类，自动添加使用viewModel
+    /// viewModel自动注入，需要提前注册 ViewModel到diService
     /// </summary>
     /// <typeparam name="TVm">viewModel类型</typeparam>
     public abstract class BaseUserControl<TVm> : UserControl where TVm : BaseViewModel {
-        protected BaseUserControl(TVm viewModel) {
+        protected BaseUserControl() {
             // 自动注入viewmodel
-            DataContext = viewModel;
+            DataContext = DiService.GetRequiredService<TVm>();
         }
 
         /// <summary>
-        /// 当前view的viewModel
+        /// 线程安全的获取当前view的viewModel
+        /// 如果不存在则返回null
         /// </summary>
-        /// <exception cref="Exception"></exception>
         protected TVm ViewModel {
             get {
                 if (Dispatcher.CheckAccess()) {
