99c5f185514ad4f738bb913be5141aaa0588808b
love-yuri <yuri2078170658@gmail.com>
2025-05-09 14:42:05 +0800

    feat(NotificationService): 优化代码实现，支持鼠标悬浮暂停通知自动关闭

--- Core/Notification/NotificationService.cs
+++ Core/Notification/NotificationService.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Shell;
@@ -14,14 +16,16 @@ namespace LoveYuri.Core.Notification {
         private const int DefaultDuration = 3000;
         private Panel container;
         private readonly Window window;
+        private readonly List<NotificationMessage> activeNotifications = new List<NotificationMessage>();
+        private bool isMouseOverAnyNotification;
 
+        /// <summary>
+        /// 创建通知服务实例
+        /// </summary>
+        /// <param name="window">关联的窗口</param>
         public NotificationService(Window window) {
             this.window = window;
-            if (!window.Dispatcher.CheckAccess()) {
-                window.Dispatcher.Invoke(Init);
-            } else {
-                Init();
-            }
+            window.Dispatcher.InvokeAsync(Init);
         }
 
         private void Init()
@@ -59,6 +63,26 @@ namespace LoveYuri.Core.Notification {
         }
 
         /// <summary>
+        /// 暂停所有通知的自动关闭计时器
+        /// </summary>
+        private void PauseAllTimers()
+        {
+            foreach (var notification in activeNotifications.Where(notification => notification.CloseTimer != null && notification.CloseTimer.IsEnabled)) {
+                notification.CloseTimer.Stop();
+            }
+        }
+
+        /// <summary>
+        /// 恢复所有通知的自动关闭计时器
+        /// </summary>
+        private void ResumeAllTimers()
+        {
+            foreach (var notification in activeNotifications.Where(notification => notification.CloseTimer != null && !notification.CloseTimer.IsEnabled)) {
+                notification.CloseTimer.Start();
+            }
+        }
+
+        /// <summary>
         /// 展示通知
         /// </summary>
         /// <param name="message">消息</param>
@@ -72,15 +96,29 @@ namespace LoveYuri.Core.Notification {
             int duration = DefaultDuration
         ) {
             // 异步通知
-            window.Dispatcher.BeginInvoke(new Action(() => {
+            window.Dispatcher.InvokeAsync(() => {
                 var notification = NotificationMessage.GetNotificationMessage(message, type);
+                
+                // 添加鼠标进入事件处理
+                notification.MouseEnter += (sender, args) => {
+                    isMouseOverAnyNotification = true;
+                    PauseAllTimers();
+                };
+                
+                // 添加鼠标离开事件处理
+                notification.MouseLeave += (sender, args) => {
+                    isMouseOverAnyNotification = false;
+                    ResumeAllTimers();
+                };
 
-                // 添加到容器
+                // 添加到容器和活动通知列表
                 container.Children.Add(notification);
+                activeNotifications.Add(notification);
                 
                 // 关联关闭
                 notification.Closed += (sender, args) => {
                     container.Children.Remove(notification);
+                    activeNotifications.Remove(notification);
                 };
                 
                 // 自动关闭 
@@ -96,9 +134,12 @@ namespace LoveYuri.Core.Notification {
                         notification.BeginFadeOutAnimation();
                     };
                 
-                    timer.Start();
+                    // 如果当前没有鼠标悬浮在任何通知上，则启动计时器
+                    if (!isMouseOverAnyNotification) {
+                        timer.Start();
+                    }
                 }
-            }));
+            });
         }
     }
 }
