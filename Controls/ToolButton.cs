794d66b8cf7a56c0449064e7613ea1a2616b30fb
love-yuri <yuri2078170658@gmail.com>
2025-07-30 10:19:07 +0800

    feat: 增加ToolButton控件，支持自定义背景和加载状态

--- Controls/ToolButton.cs
+++ Controls/ToolButton.cs
@@ -4,21 +4,30 @@ using System.Windows.Controls;
 namespace LoveYuri.Controls;
 
 /// <summary>
-/// 操作按钮，是自动应用样式的Button，支持加载状态
+///     操作按钮，是自动应用样式的Button，支持加载状态
+/// </summary>
+/// <summary>
+///     扩展的ToolButton控件，支持自定义背景和加载状态
 /// </summary>
 public class ToolButton : Button {
+    // 加载状态依赖属性
     public static readonly DependencyProperty IsLoadingProperty =
-        DependencyProperty.Register(nameof(IsLoading), typeof(bool), typeof(ToolButton),
-            new PropertyMetadata(false));
+        DependencyProperty.Register(
+            nameof(IsLoading),
+            typeof(bool),
+            typeof(ToolButton),
+            new PropertyMetadata(false)
+        );
 
-    static ToolButton() {
-        DefaultStyleKeyProperty.OverrideMetadata(typeof(ToolButton),
-            new FrameworkPropertyMetadata(typeof(ToolButton)));
+    static ToolButton()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(
+            typeof(ToolButton),
+            new FrameworkPropertyMetadata(typeof(ToolButton))
+        );
     }
 
-    /// <summary>
-    /// 是否处于加载状态
-    /// </summary>
+
     public bool IsLoading {
         get => (bool)GetValue(IsLoadingProperty);
         set => SetValue(IsLoadingProperty, value);
