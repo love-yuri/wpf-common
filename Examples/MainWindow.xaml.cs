d5bc1a7cee8284758b8d0f2b9867b17a47e50d4b
love-yuri <yuri2078170658@gmail.com>
2025-08-06 11:37:51 +0800

    feat: 添加示例项目并实现基础查询功能测试

---
+++ Examples/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using LoveYuri.Core.Sql;
+using LoveYuri.Examples.Entity;
+using LoveYuri.Utils;
+
+namespace LoveYuri.Examples;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow  {
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Test1(object sender, RoutedEventArgs e)
+    {
+        var query = QueryWrapper<SysComponents>.Builder()
+            .Eq(k => k.ComID, 1)
+            .In(k => k.ComID, [2, 3, 4, 5, 6], LogicalOperatorType.Or)
+            .OrderBy(k => k.ComID)
+            .OrderByDesc(k => k.ComName);
+
+
+        Log.Info($"{query.BuildSql()}");
+        var sp = query.Select();
+        sp.ForEach(k => {
+            Log.Info($"id: {k.ComID} name: {k.ComName}");
+        });
+    }
+}
