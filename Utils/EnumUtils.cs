1eef8d883410f197ecdbfa96c0310b42de0ead38
love-yuri <yuri2078170658@gmail.com>
2025-08-18 17:07:53 +0800

    feat: 添加枚举工具类，支持获取枚举描述

---
+++ Utils/EnumUtils.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel;
+using System.Reflection;
+
+namespace LoveYuri.Utils;
+
+/// <summary>
+/// 枚举工具
+/// </summary>
+public static class EnumUtils {
+    /// <summary>
+    /// 获取枚举的Description属性，如果不存在则返回toString
+    /// </summary>
+    private static string GetDescription(this Enum value)
+    {
+        var field = value.GetType().GetField(value.ToString());
+        var attribute = field?.GetCustomAttribute<DescriptionAttribute>();
+        return attribute?.Description ?? value.ToString();
+    }
+}
