1529aef3ee400534ea483a51267b36e9ac5820ea
love-yuri <yuri2078170658@gmail.com>
2025-09-16 16:05:38 +0800

    feat: 修改类名且支持任意类调用

--- Core/Notification/NotificationDecorator.cs
+++ Core/Notification/NotificationDecorator.cs
@@ -2,12 +2,14 @@ using System.Runtime.CompilerServices;
 using System.Windows;
 using LoveYuri.Utils;
 
+// ReSharper disable MemberCanBePrivate.Global
+
 namespace LoveYuri.Core.Notification;
 
 /// <summary>
 /// 通知装饰器，提供在WPF窗口上显示各种类型通知的扩展方法
 /// </summary>
-public static class NotificationDecorator
+public static class Notification
 {
     private static readonly ConditionalWeakTable<Window, NotificationService> Services = new();
 
@@ -27,49 +29,49 @@ public static class NotificationDecorator
     /// <summary>
     /// 在目标window上通知成功消息
     /// </summary>
-    /// <param name="window">目标窗口</param>
     /// <param name="message">消息</param>
     /// <param name="autoClose">是否自动关闭-默认为true</param>
     /// <param name="duration">持续时间-默认2s</param>
-    public static void NotifySuccess(this Window window, string message, bool autoClose = true, int duration = 2000)
+    // ReSharper disable once InvalidXmlDocComment
+    public static void NotifySuccess(this object? _, string message, bool autoClose = true, int duration = 2000)
     {
-        window.GetNotificationService().ShowNotification(message, NotificationType.Success, autoClose, duration);
+        NotifySuccess(message, autoClose, duration);
     }
 
     /// <summary>
     /// 在目标window上通知信息消息
     /// </summary>
-    /// <param name="window">目标窗口</param>
     /// <param name="message">消息</param>
     /// <param name="autoClose">是否自动关闭-默认为true</param>
     /// <param name="duration">持续时间-默认2s</param>
-    public static void NotifyInfo(this Window window, string message, bool autoClose = true, int duration = 2000)
+    // ReSharper disable once InvalidXmlDocComment
+    public static void NotifyInfo(this object? _, string message, bool autoClose = true, int duration = 2000)
     {
-        window.GetNotificationService().ShowNotification(message, NotificationType.Info, autoClose, duration);
+        NotifyInfo(message, autoClose, duration);
     }
 
     /// <summary>
     /// 在目标window上通知警告消息
     /// </summary>
-    /// <param name="window">目标窗口</param>
     /// <param name="message">消息</param>
     /// <param name="autoClose">是否自动关闭-默认为true</param>
     /// <param name="duration">持续时间-默认3s</param>
-    public static void NotifyWarning(this Window window, string message, bool autoClose = true, int duration = 3000)
+    // ReSharper disable once InvalidXmlDocComment
+    public static void NotifyWarning(this object? _, string message, bool autoClose = true, int duration = 3000)
     {
-        window.GetNotificationService().ShowNotification(message, NotificationType.Warning, autoClose, duration);
+        NotifyWarning(message, autoClose, duration);
     }
 
     /// <summary>
     /// 在目标window上通知错误消息
     /// </summary>
-    /// <param name="window">目标窗口</param>
     /// <param name="message">消息</param>
     /// <param name="autoClose">是否自动关闭-默认为true</param>
     /// <param name="duration">持续时间-默认3s</param>
-    public static void NotifyError(this Window window, string message, bool autoClose = true, int duration = 3000)
+    // ReSharper disable once InvalidXmlDocComment
+    public static void NotifyError(this object? _, string message, bool autoClose = true, int duration = 3000)
     {
-        window.GetNotificationService().ShowNotification(message, NotificationType.Error, autoClose, duration);
+        NotifyError(message, autoClose, duration);
     }
 
     /// <summary>
